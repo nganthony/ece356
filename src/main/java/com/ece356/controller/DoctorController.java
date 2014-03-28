c64ece309fb30184ae576bd20def7e559b19d27b
Amir Behnam <abehnam@uwaterloo.ca>
2014-03-28 16:55:45 -0400

    permissions for doctor's pages

--- src/main/java/com/ece356/controller/DoctorController.java
+++ src/main/java/com/ece356/controller/DoctorController.java
@@ -33,95 +33,116 @@ public class DoctorController {
 	@RequestMapping(value = "{doctorId}/patients", method= RequestMethod.GET)
 	public String showPatientsPage(@PathVariable("doctorId") int doctorId, HttpServletRequest request,
 			HttpServletResponse response, HttpSession session, Model model) throws Exception {
-		
-		User user = (User)session.getAttribute("user");
-		model.addAttribute(user);
-		
-		List<Patient> defaultPatients = patientService.getAllPatients(user.getId());
-		List<Patient> visitedPatients = visitService.getVisitedPatients(user.getId());
-		
-		model.addAttribute("defaultPatients", defaultPatients);
-		model.addAttribute("visitedPatients", visitedPatients);
-		
-		return "doctorPatients";
+		if (Util.isValidDoctor(session)) {
+			User user = (User)session.getAttribute("user");
+			model.addAttribute(user);
+			
+			List<Patient> defaultPatients = patientService.getAllPatients(user.getId());
+			List<Patient> visitedPatients = visitService.getVisitedPatients(user.getId());
+			
+			model.addAttribute("defaultPatients", defaultPatients);
+			model.addAttribute("visitedPatients", visitedPatients);
+
+			return "doctorPatients";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/patients", method= RequestMethod.POST)
 	public String showFilteredPatientsPage(@PathVariable("doctorId") int doctorId, HttpServletRequest request,
 			HttpServletResponse response, HttpSession session, Model model) throws Exception {
-		
-		User user = (User)session.getAttribute("user");
-		model.addAttribute(user);
-		
-		String search = request.getParameter("search");
-		
-		List<Patient> defaultPatients = patientService.getAllPatients(user.getId(), search);
-		List<Patient> visitedPatients = visitService.getVisitedPatients(user.getId(), search);
-		model.addAttribute("defaultPatients", defaultPatients);
-		model.addAttribute("visitedPatients", visitedPatients);
-		
-		model.addAttribute("search", search);
-		
-		return "doctorPatients";
+		if (Util.isValidDoctor(session)) {
+			User user = (User)session.getAttribute("user");
+			model.addAttribute(user);
+			
+			String search = request.getParameter("search");
+			
+			List<Patient> defaultPatients = patientService.getAllPatients(user.getId(), search);
+			List<Patient> visitedPatients = visitService.getVisitedPatients(user.getId(), search);
+			model.addAttribute("defaultPatients", defaultPatients);
+			model.addAttribute("visitedPatients", visitedPatients);
+			
+			model.addAttribute("search", search);
+			
+			return "doctorPatients";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/appointments", method= RequestMethod.GET)
-	public String showAppointmentsPage(@PathVariable("doctorId") int doctorId, Model model) {
-		
-		List<Visit> visits  = visitService.getDoctorSchedule(doctorId);
-		model.addAttribute("visits", visits);
-		return "doctorAppointments";
+	public String showAppointmentsPage(@PathVariable("doctorId") int doctorId, Model model,HttpSession session) {
+		if (Util.isValidDoctor(session)) {
+			List<Visit> visits = visitService.getDoctorSchedule(doctorId);
+			model.addAttribute("visits", visits);
+			return "doctorAppointments";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/appointments", method= RequestMethod.POST)
 	public String showFilteredAppintmentsPage(@PathVariable("doctorId") int doctorId, HttpServletRequest request,
 			HttpServletResponse response, HttpSession session, Model model) {
-		
-		String search = request.getParameter("search");
-		
-		List<Visit> visits  = visitService.getDoctorSchedule(doctorId, search);
-		model.addAttribute("visits", visits);
-		model.addAttribute("search", search);
-		model.addAttribute("doctorId", doctorId);
-		
-		return "doctorAppointments";
+		if (Util.isValidDoctor(session)) {
+			String search = request.getParameter("search");
+
+			List<Visit> visits = visitService.getDoctorSchedule(doctorId, search);
+			model.addAttribute("visits", visits);
+			model.addAttribute("search", search);
+			model.addAttribute("doctorId", doctorId);
+
+			return "doctorAppointments";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/update_appointment/{visitId}", method = RequestMethod.GET)
-	public String showUpdateAppointmentPage(@PathVariable int visitId, Model model) {
-		Visit visit = visitService.getVisit(visitId);
-		model.addAttribute("visit", visit);
-		return "doctorUpdateAppointment";
+	public String showUpdateAppointmentPage(@PathVariable int visitId, Model model, HttpSession session) {
+		if (Util.isValidDoctor(session)) {
+			Visit visit = visitService.getVisit(visitId);
+			model.addAttribute("visit", visit);
+			return "doctorUpdateAppointment";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/update_appointment/{visitId}", method = RequestMethod.POST)
-	public String updateAppointment(@PathVariable int doctorId, @PathVariable int visitId, @ModelAttribute Visit visit, Model model) {
-		visit.setUser_id(doctorId);
-		visit.setId(visitId);
-		visitService.updateForDoctor(visit);
-		
-		return "redirect:/doctor/" + doctorId + "/appointments";
+	public String updateAppointment(@PathVariable int doctorId, @PathVariable int visitId, @ModelAttribute Visit visit, Model model, HttpSession session) {
+		if (Util.isValidDoctor(session)) {
+			visit.setUser_id(doctorId);
+			visit.setId(visitId);
+			visitService.updateForDoctor(visit);
+
+			return "redirect:/doctor/" + doctorId + "/appointments";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/patient/{healthCard}/records", method = RequestMethod.GET)
-	public String getPatientRecord(@PathVariable("doctorId") int doctorId, @PathVariable("healthCard") String healthCard, Model model) {
-		
-		Patient patient = patientService.findByHealthCard(healthCard);
-		List<Visit> visits = visitService.getPatientVisit(healthCard, doctorId);
-		
- 		model.addAttribute("patient", patient);
-		model.addAttribute("visits", visits);
-		
-		return "doctorPatientRecordView";
+	public String getPatientRecord(@PathVariable("doctorId") int doctorId, @PathVariable("healthCard") String healthCard, Model model, HttpSession session) {
+		if (Util.isValidDoctor(session)) {
+			Patient patient = patientService.findByHealthCard(healthCard);
+			List<Visit> visits = visitService.getPatientVisit(healthCard, doctorId);
+			User defaultDoctor = patientService.getDefaultDoctor(patient);
+			
+			model.addAttribute("defaultDoctor", defaultDoctor.getFirstName()
+					+ " " + defaultDoctor.getLastName());
+			model.addAttribute("patient", patient);
+			model.addAttribute("visits", visits);
+
+			return "doctorPatientRecordView";
+		}
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value = "{doctorId}/patient/{healthCard}/permissions", method = RequestMethod.GET)
-	public String getPatientPermissions(@PathVariable("doctorId") int doctorId, @PathVariable("healthCard") String healthCard, Model model) {
-		
-		Patient patient = patientService.findByHealthCard(healthCard);	
- 		model.addAttribute("patient", patient);
+	public String getPatientPermissions(@PathVariable("doctorId") int doctorId, @PathVariable("healthCard") String healthCard, Model model , HttpSession session) {
+		if (Util.isValidDoctor(session)) {
+			Patient patient = patientService.findByHealthCard(healthCard);
+			model.addAttribute("patient", patient);
 
-		return "doctorPatientPermissionView";
+			return "doctorPatientPermissionView";
+		}
+		return "redirect:/";
 	}
 	
 }
